a889739280d85f102f0712bef913c826859f839e
kjsmobiledognailservices <kjsmobiledognailservices@gmail.com>
2025-08-15 14:17:35 -0500

    Add files via upload

--- appointments/sw.js
+++ appointments/sw.js
@@ -1,36 +1,4 @@
-const CACHE = 'kj-appointments-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
-];
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
-});
-
-self.addEventListener('fetch', (e)=>{
-  const url = new URL(e.request.url);
-  if (ASSETS.includes(url.pathname.replace(/.*\//, './'))) {
-    e.respondWith(caches.match(e.request));
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request).then(resp=>{
-      const clone = resp.clone();
-      caches.open(CACHE).then(c=>c.put(e.request, clone));
-      return resp;
-    }).catch(()=> caches.match('./index.html')))
-  );
-});
+const CACHE='kj-appointments-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request).then(resp=>{const clone=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return resp;}).catch(()=>caches.match('./index.html'))))});
